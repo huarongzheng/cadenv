a8ba026ff1fa1139b30db2357895f8b9ad40a237
huarongzheng <chauviner@hoitmail.com>
2016-04-12 19:22:07 -0700

    chmod +x  and remove dummy comment

---
+++ scripts/sshauto.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/expect -f
+
+#autossh.sh oxterm-81 huarong new4rit
+if { $argc < 3 } {
+puts stderr "Usage: $argv0 IPAdress Login OldPasswd"
+exit
+}
+
+set IPADDR [lindex $argv 0]
+set LOGIN [lindex $argv 1]
+set OLD_PW [lindex $argv 2]
+
+set timeout 30
+
+stty -echo
+
+spawn ssh $IPADDR -l $LOGIN
+expect {
+    "*assword:*"  {
+        send "$OLD_PW\r"
+        exp_continue
+    } "*Last login:*" {
+        interact
+        exit 0
+    } timeout {
+        send_user "connection to $IPADDR timeout!\n"
+        exit 1
+    } "*incorrect*" {
+        send_user "password incorrect!\n"
+        exit 2
+    } "*Permission*" {  #for LINUX ssh
+        send_user "password Error!\n"
+        exit 2
+    } eof {
+        exit 3
+    }
+}
