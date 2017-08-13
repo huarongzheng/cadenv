be9c77635bb650ff0ae274e8fc698fc2595a5c55
huarongzheng <chauviner@hotmail.com>
2017-08-13 21:06:47 +0800

    calc lost (negative return)

--- scripts/finLib.py
+++ scripts/finLib.py
@@ -35,38 +35,57 @@ def setup_logging(
 # @fBondYield: yield rate for discounting cash flow within bond maturity
 # @fCouponRate: anual coupon rate
 # @fCouponInterval: coupon paying date, semianual-0.5 anual-1 etc
+# @fDefaultPont: the date when default happens, in the unit of year with 0 as now 
 # @fMaturity: maturity of bound
 # @couponDate: derived from fCouponInterval if couponDate param is not specifically provided
 # @return: current value of the bond 
-def BondValue(fBondYield, fCouponRate, fCouponInterval, fMaturity, uFaceValue=100, couponDate = None):
+def BondValue(fBondYield, fCouponRate, fCouponInterval, fMaturity, fDefaultPoint=None, fFaceValue=100, couponDate=None):
     logger = logging.getLogger(__name__)
     if couponDate == None:
         couponDate = np.arange(fCouponInterval, fMaturity+fCouponInterval, fCouponInterval)
-    logger.info('couponDate=%s', couponDate)
     discountRate = np.exp(-fBondYield*couponDate)
     logger.debug('discountRate=%s', discountRate)
 
-    fCoupon = fCouponInterval * fCouponRate * uFaceValue
+    fCoupon = fCouponInterval * fCouponRate * fFaceValue
     ucashFlowSize = couponDate.size
     cashFlow = np.ones(ucashFlowSize)*fCoupon
-    cashFlow[ucashFlowSize - 1] += uFaceValue
+    cashFlow[ucashFlowSize - 1] += fFaceValue
     logger.debug('cashFlow=%s', cashFlow)
+    if fDefaultPoint != None:
+        for i in range(0, couponDate.shape[0]):
+            if couponDate[i]>=fDefaultPoint:
+                cashFlow[i] = 0 # nullify defaulted return
+    logger.debug('nullified cashFlow=%s', cashFlow)
 
     discountedCashFlow = discountRate * cashFlow
     logger.debug('discountedCashFlow=%s', discountedCashFlow)
 
-    logger.debug('boundValue=%s', np.sum(discountedCashFlow))
+    logger.info('%.2f=BondValue(fBondYield=%.2f, fCouponRate=%.2f, fCouponInterval=%.2f, fMaturity=%.2f, fDefaultPoint=%s, fFaceValue=%.2f, couponDate=%s)', np.sum(discountedCashFlow), fBondYield, fCouponRate, fCouponInterval, fMaturity, fDefaultPoint, fFaceValue, couponDate)
+
     return np.sum(discountedCashFlow)
 
 def main():
     setup_logging(logging.DEBUG)
+    fRecoveryValue = 100*0.4
+    fRiskFreeRate = 0.05
 
     #bondYield  = np.array([0.065, 0.068, 0.0695])
-    fBondYield  = 0.05
+    fBondYield  = fRiskFreeRate
     fCouponRate = 0.08
     fCouponInterval = 0.5
     fMaturity   = 1
-    BondValue(fBondYield, fCouponRate, fCouponInterval, fMaturity)
+    defaultPoint = np.array([0.25, 0.75])
+    fdefaulReturn = np.zeros(defaultPoint.size)
+    for i in range(0, defaultPoint.size):
+        """current value of the recovery on default date + 
+           current value of all the risk free return before default -
+           current value of all the risk free return
+        """
+        fdefaulReturn[i] = \
+            fRecoveryValue*np.exp(-fRiskFreeRate*defaultPoint[i])  + \
+            BondValue(fRiskFreeRate, fCouponRate, fCouponInterval, fMaturity, defaultPoint[i]) - \
+            BondValue(fRiskFreeRate, fCouponRate, fCouponInterval, fMaturity)
+    logging.info('fdefaulReturn=%s', fdefaulReturn)
 
 if __name__ == "__main__":
     main()
