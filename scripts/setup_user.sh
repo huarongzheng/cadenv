1512aa5ee6525cb885764ba6e34224792aa0009f
zion <zion@splendor.top>
2022-06-30 05:52:14 +0800

    setup user

--- scripts/setup_user.sh
+++ scripts/setup_user.sh
@@ -1,60 +1,54 @@
 #!/bin/bash
 
-#sudo useradd -g devel -s /usr/bin/zsh -m fredg 
-#sudo passwd
-
 print_usage () {
     echo "####################################################################"
-    echo "## Usage:  setup_user.sh user_name group_name default_shell"
+    echo "## Usage:  setup_user.sh user_name group_name"
     echo "####################################################################"
 }
 
-if [ $# -lt 3 ]; then
+if [ $# -lt 2 ]; then
     echo "ERROR: expecting 3 parameters but get only $#"
     print_usage 
     exit 1
 else
     USER_NAME=$1
     GROUP_NAME=$2
-    SHELL_NAME=$3 # should like /bin/tcsh
 fi
 
-#UTILS_CADENV=/proot/workareas/utils
+SHELL_NAME=/usr/bin/zsh
+UTILS_CADENV=/home/tools/proot
 
 ###########  for others  ###########
-GROUP_INFO=`getent group | grep $GROUP_NAME | awk -F ":" -v groupname=$GROUP_NAME '{if ($1==groupname) {print $1}}'`
-if [[ "$GROUP_INFO" == "" && "$USER_NAME" != "root" ]]; then
+GROUP_INFO=`getent group | grep "^$GROUP_NAME:"`
+if [[ "$GROUP_INFO" == "" ]]; then
     echo "INFO: group $GROUP_NAME doesn't exist, creating one ..."
     groupadd $GROUP_NAME
+else
+    echo "INFO: exist $GROUP_INFO"
 fi
 
-USER_INFO=`getent passwd | grep $USER_NAME`
-if [[ "$USER_INFO" == "" && "$USER_NAME" != "root" ]]; then
+USER_INFO=`getent passwd | grep "^$USER_NAME:"`
+if [[ "$USER_INFO" == "" ]]; then
     echo "INFO: user $USER_NAME doesn't exist, creating one ..."
     useradd -g $GROUP_NAME -s $SHELL_NAME -m $USER_NAME
+    groups $USER_NAME # show which group for this user
+    #chsh -s $SHELL_NAME $USER_NAME
+else
+    #usermod -a -G $GROUP_NAME $USER_NAME # add user to the group
+    echo "INFO: exist $USER_INFO"
 fi
 
 if [ "$USER_NAME" != "root" ]; then
     HOME_PATH="/home"
 else
-    HOME_PATH=
+    HOME_PATH="/"
 fi
 
-chsh -s $SHELL_NAME $USER_NAME
-
-ln -sf $UTILS_CADENV/settings/ohmyzsh
-ln -sf $UTILS_CADENV/settings/.vim
-ln -sf $UTILS_CADENV/settings/.vimrc
-ln -sf $UTILS_CADENV/settings/.zshrc
-ln -sf $UTILS_CADENV/settings/.localrc
-ln -sf $UTILS_CADENV/settings/.gitconfig
-ln -sf $UTILS_CADENV/settings/.aliasesb
-
-usermod -G $GROUP_NAME $USER_NAME
-
-GROUP_INFO=`getent group | grep $USER_NAME | awk -F ":" '{print $1}'`
-if [[ "$GROUP_INFO" == "$USER_NAME" && "$GROUP_INFO" != "root" ]]; then
-    echo "INFO: group: $USER_NAME is redundant, deleting ..."
-    groupdel $USER_NAME
-fi
+ln -sf $UTILS_CADENV/ohmyzsh                    $HOME_PATH/$USER_NAME/ohmyzsh
+ln -sf $UTILS_CADENV/canenv/settings/.aliasesb  $HOME_PATH/$USER_NAME/.aliasesb
+ln -sf $UTILS_CADENV/canenv/settings/.gitconfig $HOME_PATH/$USER_NAME/.gitconfig 
+ln -sf $UTILS_CADENV/canenv/settings/.localrc   $HOME_PATH/$USER_NAME/.localrc
+ln -sf $UTILS_CADENV/canenv/settings/.zshrc     $HOME_PATH/$USER_NAME/.zshrc
+ln -sf $UTILS_CADENV/canenv/settings/.vim       $HOME_PATH/$USER_NAME/.vim
+ln -sf $UTILS_CADENV/canenv/settings/.vimrc     $HOME_PATH/$USER_NAME/.vimrc
 
